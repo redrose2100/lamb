8b06c8b52cc511101fd5386f840245098acb3f95
honghua <honghua@iscas.ac.cn>
2024-03-27 13:11:40 +0800

    update init pkg

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python3-sheep
-a advance layer abstract of python usage, aim to supply a more easy and useful lib
+an advance layer abstract of python usage, aim to supply a more easy and useful lib
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="python3-sheep",
+    version="0.0.1",
+    description="advance lib of python usage.",
+    author="redrose2100",
+    author_email="hitredrose@163.com",
+    maintainer="redrose2100",
+    maintainer_email="hitredrose@163.com",
+    requires=[
+    ],
+    license="MIT",
+    package=find_packages(),
+)
