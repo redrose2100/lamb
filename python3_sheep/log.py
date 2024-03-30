cd729669240b8950d7520b422be0a02452f5b68c
honghua <honghua@iscas.ac.cn>
2024-03-30 14:25:36 +0800

    add log

---
+++ python3_sheep/log.py
@@ -0,0 +1,28 @@
+import logging
+import platform
+from pathlib import Path
+from concurrent_log_handler import ConcurrentRotatingFileHandler
+
+def get_logger(name,level=logging.INFO,log_path=None):
+    sys_name=platform.system()
+    if not log_path:
+        if sys_name == "Linux":
+            log_path=f"/var/log/{name}/python3_sheep.log"
+        else:
+            log_path = Path(__file__).parent / "python3_sheep.log"
+    handler = ConcurrentRotatingFileHandler(log_path, "a", 512 * 1024, 30)
+    handler.setFormatter(logging.Formatter('%(asctime)s | %(levelname)s | %(filename)s:%(lineno)s | %(message)s'))
+    logger = logging.getLogger(name)
+    logger.addHandler(handler)
+    logger.setLevel(level)
+    logger.propagate = False
+    return logger
+
+log=get_logger("python3_sheep")
+
+if __name__=="__main__":
+    log=get_logger("redrose2100")
+    log.debug("test debug log")
+    log.info("test info log")
+    log.warning("test warning log")
+    log.error("test error log")
--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="python3-sheep",
+    name="python3_sheep",
     version="0.0.1",
     description="advance lib of python usage.",
     author="redrose2100",
     author_email="hitredrose@163.com",
     maintainer="redrose2100",
     maintainer_email="hitredrose@163.com",
-    requires=[
+    install_require=[
+        "concurrent-log-handler"
     ],
     license="MIT",
     package=find_packages(),
