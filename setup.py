3988c49f0060a33c87147be1c0f226d51f3b77a3
honghua <honghua@iscas.ac.cn>
2024-03-30 15:04:41 +0800

    update proj name

--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name="python3_sheep",
+    name="python3_lamb",
     version="0.0.1",
     description="advance lib of python usage.",
     author="redrose2100",
     author_email="hitredrose@163.com",
     maintainer="redrose2100",
     maintainer_email="hitredrose@163.com",
+    url="https://github.com/redrose2100/python3_lamb",
     install_require=[
         "concurrent-log-handler"
     ],
