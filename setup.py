0f932d7f26e575cd7ce1d57f1cf5530437eb0a9f
honghua <honghua@iscas.ac.cn>
2024-03-30 15:27:45 +0800

    fix log

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="python3_lamb",
-    version="0.0.1",
+    version="0.0.2",
     description="advance lib of python usage.",
     author="redrose2100",
     author_email="hitredrose@163.com",
