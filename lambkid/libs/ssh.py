78909473784047d1237988c96bf0bb57efeecd31
honghua <honghua@iscas.ac.cn>
2024-04-17 14:10:39 +0800

    update ssh

--- lambkid/libs/ssh.py
+++ lambkid/libs/ssh.py
@@ -68,7 +68,6 @@ class SSHClient(object):
                 return False
 
     def exec(self, cmd, timeout=600,max_attempts=1):
-        cmd_new=f"max_attempts={max_attempts}; attempts=0; until {cmd} || (( attempts++ >= max_attempts )); do echo \"Attempt $attempts of $max_attempts\"; sleep 1; done"
         log.info(f" {self.__ip}:{self.__port} | begin to run cmd {cmd}, timeout is {timeout}...")
         def _exec(cmd):
             try:
@@ -90,7 +89,7 @@ class SSHClient(object):
                 return rs
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            future = executor.submit(_exec,cmd_new)
+            future = executor.submit(_exec,cmd)
             try:
                 result = future.result(timeout=timeout)
                 return result
