83fb9bb4828a73bcc06515383ac9bf6f747a5fe0
honghua <honghua@iscas.ac.cn>
2024-05-22 10:45:22 +0800

    update log

--- lambkid/__init__.py
+++ lambkid/__init__.py
@@ -1,4 +1,3 @@
-from lambkid.libs.log import log
 from lambkid.libs.log import get_logger
 from lambkid.libs.ssh import SSHClient
 from lambkid.libs.utils import read_csv,write_csv,get_all_files,read_json
